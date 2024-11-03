b5b06b403fca861cf4e58a526edbcb38954908b5
Anas Salha <anassalha96@gmail.com>
2024-11-04 00:23:14 +0100

    ticket_v1: Solve 12_outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,70 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    fn validate_product_name(name: &String) {
+        if name.is_empty() || name.len() > 300 {
+            panic!();
+        }
+    }
+
+    fn validate_quantity(quantity: &u32) {
+        if *quantity <= 0 {
+            panic!();
+        }
+    }
+    fn validate_unit_price(price: &u32) {
+        if *price <= 0 {
+            panic!();
+        }
+    }
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity.saturating_mul(self.unit_price)
+    }
+
+    pub fn set_product_name(&mut self, name: String) {
+        Self::validate_product_name(&name);
+        self.product_name = name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Self::validate_quantity(&quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, price: u32) {
+        Self::validate_unit_price(&price);
+        self.unit_price = price;
+    }
+}
