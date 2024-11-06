9609a0f0b8c2d9abc84ff1bab8de3b1edc3f1580
Anas Salha <anassalha96@gmail.com>
2024-11-06 19:27:57 +0100

    traits: Solve 14_outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,80 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(number: u16) -> Self {
+        SaturatingU16 { value: number }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(number: &u16) -> Self {
+        SaturatingU16 { value: *number }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(number: u8) -> Self {
+        SaturatingU16 {
+            value: number.into(),
+        }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(number: &u8) -> Self {
+        SaturatingU16 {
+            value: (*number).into(),
+        }
+    }
+}
+
+impl Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let result = self.value.saturating_add(rhs.value);
+        SaturatingU16 { value: result }
+    }
+}
+
+impl Add<&Self> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self::Output {
+        let result = self.value.saturating_add(rhs.value);
+        SaturatingU16 { value: result }
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        let result = self.value.saturating_add(rhs);
+        SaturatingU16 { value: result }
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        let result = self.value.saturating_add(*rhs);
+        SaturatingU16 { value: result }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
