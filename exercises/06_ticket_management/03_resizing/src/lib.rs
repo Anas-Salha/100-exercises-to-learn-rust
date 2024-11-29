d13184158d708c7ba7fc59487b7627ed8d78577d
Anas Salha <anassalha96@gmail.com>
2024-11-29 15:15:40 +0100

    ticket_management: Solve 03_resizing
    Looks like the current approach is to double size for every resize

--- exercises/06_ticket_management/03_resizing/src/lib.rs
+++ exercises/06_ticket_management/03_resizing/src/lib.rs
@@ -12,6 +12,16 @@ mod tests {
         // Can you guess what the new capacity will be?
         // Beware that the standard library makes no guarantees about the
         // algorithm used to resize the vector, so this may change in the future.
-        assert_eq!(v.capacity(), todo!());
+        assert_eq!(v.capacity(), 4);
+
+        v.push(4);
+        v.push(5); // beyond capacity, needs to resize
+        assert_eq!(v.capacity(), 8);
+
+        v.push(6);
+        v.push(7);
+        v.push(8);
+        v.push(9); // beyond capacity, needs to resize
+        assert_eq!(v.capacity(), 16);
     }
 }
